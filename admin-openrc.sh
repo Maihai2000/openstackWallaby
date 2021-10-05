d673611c001fd140384bf0f2b5873ba6bd5905c8
maihai0803@gmail.com <Daihai2000>
2021-10-05 20:27:58 +0700

    h

---
+++ admin-openrc.sh
@@ -0,0 +1,10 @@
+source config.cfg
+
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=$DEFAULT_PASS
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
